aed807d7c5ca972b6c7b22cfad60c53fc9c3269c
kr2006 <sasakravec663@gmail.com>
2019-11-27 17:51:48 +0200

    added slider and advantages section

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,20 @@
 $(function(){
 
+    //MIXITUP
     var mixer = mixitup('.products__inner-box');
 
+    //RATE
+    $(".rate-star").rateYo({
+        rating: 5,
+        readOnly: true,
+        starWidth: "12px"
+      });
 
-
+    //SLICK-SLIDER
+    $('.product-slider__inner').slick({
+        slidesToShow: 4,
+        slidesToScroll: 2,
+        dots: true,
+        arrows: false,
+    })
 });
