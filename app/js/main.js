dc170eb9deb912aa74a7b0fef8497f1f514af778
kr2006 <sasakravec663@gmail.com>
2019-12-02 21:51:00 +0200

    added new items page

--- app/js/main.js
+++ app/js/main.js
@@ -1,20 +1,44 @@
 $(function(){
 
-    //MIXITUP
+
+    $('.icon-th-list').on('click', function(){
+        $('.product__item').addClass('list');
+        $('.icon-th-list').addClass('active');
+        $('.icon-th-large').removeClass('active');
+    })
+
+    $('.icon-th-large').on('click', function(){
+        $('.product__item').removeClass('list');
+        $('.icon-th-large').addClass('active');
+        $('.icon-th-list').removeClass('active');
+    })
+
+    
+    
+    $(".js-range-slider").ionRangeSlider({
+        type: "double",
+        min: 0,
+        max: 1000,
+        from: 0,
+        to: 600,
+        prefix: "$"
+    });
+
     var mixer = mixitup('.products__inner-box');
 
-    //RATE
     $(".rate-star").rateYo({
         rating: 5,
         readOnly: true,
         starWidth: "12px"
       });
 
-    //SLICK-SLIDER
     $('.product-slider__inner').slick({
         slidesToShow: 4,
         slidesToScroll: 2,
         dots: true,
         arrows: false,
     })
+
+   
+
 });
